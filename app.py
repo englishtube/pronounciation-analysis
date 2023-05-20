daa90693dc19faa20a7c551be01635ab5e15969b
englishtube <129168257+englishtube@users.noreply.github.com>
2023-05-20 15:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import json
 import speech_score as ss
+import quiz
+import quiz2
 
 location = os.getcwd()
 audio_dir = "audios"
@@ -73,7 +75,8 @@ def pronouciation_analyse():
             json_d = {"status":"failed","error":str(e)}
             json_data=json.dumps(json_d)
             return json_data
-
+        
+# Download wav file
 @app.route('/download/analysis_audio')
 def downloadaudio():
   audio_filename = "analysis_audio.wav"
@@ -81,6 +84,78 @@ def downloadaudio():
   if os.path.isfile(path):
     return send_from_directory(audio_folder_path, audio_filename)
   return "No file found"
+
+#For Word Review1 
+@app.route("/word_quiz1",methods=['POST','GET'])
+def word_quiz1():
+    word = ""
+    json_data = ""
+    if(request.method == 'POST'):
+        word=request.form['word']
+        word_definition = quiz.meaning(word)
+        syno = quiz.synonym(word)
+        anto = quiz.antonym(word)
+        option1 = quiz.definitions(syno)
+        option2 = quiz.definitions(anto)
+        correct_answer = quiz.word_quiz(word, word_definition, option1, option2)
+        json_d = {"word" : word, "option1" : word_definition, "option2" : option1, "option3" : option2, "correct_answer" : correct_answer}
+        json_data=json.dumps(json_d)
+        print("json_data",json_data)
+    return json_data
+
+# For Word Review2 
+@app.route("/word_quiz2",methods=['POST','GET'])
+def word_quiz2():
+    chainese_word = ""
+    japanese_word = ""
+    korean_word = ""
+    json_data = ""
+    if request.method == 'POST':
+        if 'chainese_word' in request.form:
+            chainese_word = request.form['chainese_word']
+            eng_trans = quiz2.translatorch(chainese_word)
+            option1 = quiz2.synonym(eng_trans)
+            option2 = quiz2.antonym(eng_trans)
+            correct_answer = quiz2.word_quizch(chainese_word, eng_trans, option1, option2)
+            json_d = {
+                "chainese_word": chainese_word,
+                "option1": eng_trans,
+                "option2": option1,
+                "option3": option2,
+                "correct_answer": correct_answer
+            }
+            json_data=json.dumps(json_d, ensure_ascii=False).encode('utf8')
+            return json_data
+        if 'japanese_word' in request.form:
+            japanese_word = request.form['japanese_word']
+            eng_trans = quiz2.translatorja(japanese_word)
+            option1 = quiz2.synonym(eng_trans)
+            option2 = quiz2.antonym(eng_trans)
+            correct_answer = quiz2.word_quizja(japanese_word, eng_trans, option1, option2)
+            json_d = {
+                "japanese_word": japanese_word,
+                "option1": eng_trans,
+                "option2": option1,
+                "option3": option2,
+                "correct_answer": correct_answer
+            }
+            json_data=json.dumps(json_d, ensure_ascii=False).encode('utf8')
+            return json_data
+        if 'korean_word' in request.form:
+            korean_word = request.form['korean_word']
+            eng_trans = quiz2.translatorko(korean_word)
+            option1 = quiz2.synonym(eng_trans)
+            option2 = quiz2.antonym(eng_trans)
+            correct_answer = quiz2.word_quizko(korean_word, eng_trans, option1, option2)
+            json_d = {
+                "korean_word": korean_word,
+                "option1": eng_trans,
+                "option2": option1,
+                "option3": option2,
+                "correct_answer": correct_answer
+            }
+            json_data=json.dumps(json_d, ensure_ascii=False).encode('utf8')
+            return json_data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
