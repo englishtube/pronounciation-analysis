7a291b75df56cbcfd3708a2e89594fc8b7c46b4e
englishtube <129168257+englishtube@users.noreply.github.com>
2023-05-20 15:04:21 +0530

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,86 @@
+import os
+import nltk
+from nltk.corpus import wordnet as wn
+import translators as ts
+    
+def translatorch(chainese_word):
+    eng_trans = ts.translate_text(chainese_word, translator='google', from_language='zh-CN', to_language='en-US')
+    return eng_trans
+    
+def translatorja(japanese_word):
+    eng_trans = ts.translate_text(japanese_word, translator='google', from_language='ja', to_language='en-US')
+    return eng_trans
+
+def translatorko(korean_word):
+    eng_trans = ts.translate_text(korean_word, translator='google', from_language='ko', to_language='en-US')
+    return eng_trans
+
+def synonym(eng_trans):
+
+    synonyms = []
+
+    for syn in wn.synsets(eng_trans):
+        for lemma in syn.lemmas():
+            synonyms.append(lemma.name())
+
+   
+    syno=[]
+    [syno.append(x) for x in synonyms if x not in syno]
+    
+    option1 = syno[1]
+
+    return option1
+
+      
+def antonym(eng_trans):
+
+    antonyms = []
+
+    for syn in wn.synsets(eng_trans):
+        for lemma in syn.lemmas():
+            if lemma.antonyms():
+                antonyms.append(lemma.antonyms()[0].name())
+    
+    anto=[]
+    [anto.append(x) for x in antonyms if x not in anto]
+    
+    option2 = anto[0]
+
+    return option2
+    
+
+def word_quizch(chainese_word, eng_trans, option1, option2):
+    QUESTIONS = {
+        chainese_word: [
+            eng_trans, option1, option2
+        ]
+    }
+
+    for question, alternatives in QUESTIONS.items():
+        correct_answer = alternatives[0]
+        sorted_alternatives = sorted(alternatives)
+    return correct_answer
+    
+def word_quizja(japanese_word, eng_trans, option1, option2):
+    QUESTIONS = {
+        japanese_word: [
+            eng_trans, option1, option2
+        ]
+    }
+
+    for question, alternatives in QUESTIONS.items():
+        correct_answer = alternatives[0]
+        sorted_alternatives = sorted(alternatives)
+    return correct_answer
+
+def word_quizko(korean_word, eng_trans, option1, option2):
+    QUESTIONS = {
+        korean_word: [
+            eng_trans, option1, option2
+        ]
+    }
+
+    for question, alternatives in QUESTIONS.items():
+        correct_answer = alternatives[0]
+        sorted_alternatives = sorted(alternatives)
+    return correct_answer
